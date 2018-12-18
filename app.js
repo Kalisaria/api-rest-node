e7ddb9a671e7da859b5bed88b1ef5f76d1252f0c
unknown <danaamey@hotmail.com>
2018-12-18 18:28:37 +0100

    minor changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const routes = require('./routes')
  
 initializeDatabases().then(dbs => {
   // Initialize the application once database connections are ready.
-  routes(app, dbs).listen(3000, () => console.log('Listening on port 3000'))
+  routes(app, dbs).listen(4562, () => console.log('Listening on port 3000'))
 }).catch(err => {
   console.error('Failed to make all database connections!')
   console.error(err)
